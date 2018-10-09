a7b7bb22e0828b2a6d6cf798f9508f3f3c14a6e9
Chris A. Stokvik <cst040@post.uit.no>
2018-10-09 13:58:01 +0200

    .

--- Task3.R
+++ Task3.R
@@ -1,43 +1,43 @@
 library(rvest)
 library(tidyverse)
-install.packages("dplR")
 library(dplyr)
 library(stringr)
+library(lubridate)
 
 # Lese link til side med informasjon 
 
 br <- read_html("https://w2.brreg.no/kunngjoring/kombisok.jsp?datoFra=01.01.2018&datoTil=08.10.2018&id_region=100&id_fylke=19&id_kommune=-+-+-&id_niva1=51&id_niva2=-+-+-&id_bransje1=0")
 
-#Brukt selector gaget til ?? subset uthenting av data slik at jeg f??r to lister med nummer (org + per) og en med dato. 
+#Used selectorgaget to subset data, so that get a indicidual lists of numbers and dates 
 
 br_nummer <- br%>%html_nodes("td:nth-child(4) p")%>% html_text()
 br_dato <- br%>%html_nodes("tr~ tr+ tr td:nth-child(6) p")%>% html_text()
 
 
-#Fjerne mellomrom i organisasjonsnummer 
+#Remove blanckspaces 
 
-br_nummer <- str_replace_all(br_nummer, " ", "" )
+br_nummer <- str_replace_all(br_nummer, pattern = " ", replacement = "" )
 
-# Gj??re br_nummer om til numeric fra charackter 
+# Filtrere out birthdays 
 
-br_nummer <- as.numeric(br_nummer)
+br_org <- subset(br_nummer, br_nummer > 9)
 
-# Gj??re br_dato om til dato 
+# Change br_nummer to numeric from charackter 
 
-br_dato <- lubridate::dmy(br_dato)
+br_org <- as.numeric(br_org)
 
-# Filtrere ut f??dselsdatoer 
+# Change the type to date 
 
-br_org <- filter(br_nummer, numeric(length = 9L))
+br_dato2 <- as.Date(br_dato, "%d.%m.%Y")
 
 
-# Gj??re det om til et dataset 
 
-br_datasett <- data.frame(br_org, br_dato)
+# Combine the to lists 
 
+br_datasett <- cbind(br_org, br_dato)
 
 # Plot 
 
-ggplot(br_datasett, 
+ggplot(br_datasett,
 
 
